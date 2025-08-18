4a5fc30530d3db0c67d3f1d6fab6b7d437cb3319
Joan Sala <contact@joansala.com>
2025-08-18 17:59:22 +0200

    Fix benchmark suite path for tests

--- src/test/java/com/joansala/test/game/tictactoe/TicTacToeBoardTest.java
+++ src/test/java/com/joansala/test/game/tictactoe/TicTacToeBoardTest.java
@@ -13,7 +13,7 @@ import com.joansala.util.suites.SuiteReader;
 public class TicTacToeBoardTest implements BoardContract {
 
     /** Test suite file path */
-    private static String SUITE_PATH = "tictactoe-bench.suite";
+    private static String SUITE_PATH = "src/test/resources/tictactoe-bench.suite";
 
 
     /**
